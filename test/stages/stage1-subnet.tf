afcb6de8fd373b08692f49b4d3605fdb07ad7469
Sudhir Jain <sudhir.jain@ibm.com>
2022-06-09 13:17:13 +0530

    resolved issues related to build failed in PR
    Signed-off-by: Sudhir Jain <sudhir.jain@ibm.com>

--- test/stages/stage1-subnet.tf
+++ test/stages/stage1-subnet.tf
@@ -1,10 +1,10 @@
-module "subnet" {
+module "subnets" {
   source = "github.com/cloud-native-toolkit/terraform-azure-subnets"
 
   resource_group_name = module.resource_group.name
   region              = var.region
   vpc_name            = module.vnet.name
-  subnet_name= "AzureBastionSubnet"
+  subnet_name         = "AzureBastionSubnet"
   _count              = 1
   ipv4_cidr_blocks    = ["10.1.1.0/24"]
   enabled             = true
@@ -20,7 +20,7 @@ module "subnet" {
       source_port_min = 22
       source_port_max = 22
     }
-    
+
   }]
 }
 
