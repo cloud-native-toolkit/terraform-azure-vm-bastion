5c8f09da870afc7beb2fdd6b18eb62c2f13501bf
Hemang-IBM <103431190+hemang-ibm@users.noreply.github.com>
2022-07-04 07:05:39 +0530

    1 terraform azure vm bastion (#2)
    * initial changes commit
    
    initial changes commit and main.tf and variables.tf
    
    * initial resource creation code commit
    
    initial resource creation code commit
    
    * test case implementation
    
    test case implementation for vm bastion host
    
    * bastion host test case updated
    
    bastion host test case updated
    
    * test cases updated
    
    test cases updated and tested with iscalable tool module
    
    * custom subnet creation added
    
    custom subnet creation added for hardcoded name of subnet for vm bastion host
    
    * test case updated for subnet changes
    
    test case updated for subnet changes
    
    * comments updated in variable.tf
    
    comments updated in variable.tf
    
    * github action alignment to Azure
    
    Signed-off-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
    
    * Create provider.tf
    
    test/stages/provider.tf  file added.
    
    * Update stage2-bastion_host.tf
    
    module vnet name updated
    
    * subnet module related changes
    
    subnet module reused with custom name
    
    * resolved issues related to subnet_id
    
    Signed-off-by: Sudhir Jain <sudhir.jain@ibm.com>
    
    * resolved issues related to build failed in PR
    
    Signed-off-by: Sudhir Jain <sudhir.jain@ibm.com>
    
    * Update README.md
    
    README file updated for Azure vm bastion module.
    
    * ReadMe file updated
    
    Signed-off-by: hemang-ibm <103431190+hemang-ibm@users.noreply.github.com>
    
    Co-authored-by: Rich Ehrhardt <rich_ehrhardt@au1.ibm.com>
    Co-authored-by: Sudhir Jain <sudhir.jain@ibm.com>

---
+++ test/stages/stage2-bastion_host.tf
@@ -0,0 +1,13 @@
+
+module "azure_vm_bastion" {
+  source                      = "./module"
+  resource_group_name         = module.resource_group.name
+  region                      = var.region
+  bastion_host_name           = "ahm_vm_bastion"
+  ip_configuration_name       = "ahm_ip_config_bastion"
+  virtual_network_name        = module.vnet.name
+  public_ip_name              = "ahm_public_ip"
+  public_ip_allocation_method = "Static"
+  public_ip_sku               = "Standard"   
+  subnet_id                 = module.subnets.ids[0]
+}
--- variables.tf
+++ variables.tf
@@ -1,6 +1,67 @@
-#variable "myvariable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = ""
-#}
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the Azure resource group where the bastion host has been provisioned"
+}
+
+variable "region" {
+  type        = string
+  description = "The Azure location where the bastion host will be installed"
+}
+
+variable "virtual_network_name" {
+  type        = string
+  description = "The Azure virtual network name"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "The Azure subnet id"
+}
+
+
+# variable "subnet_address_space" {
+#   type        = list(string)
+#   description = "The Azure subnet address prefixes"
+# }
+
+variable "ip_configuration_name" {
+  type        = string
+  description = "The Azure ip configuration name which is used in vm bastion host"
+
+}
+
+variable "bastion_host_name" {
+  type        = string
+  description = "This is the name of Azure bastion host name"
+}
+
+variable "public_ip_name" {
+  type        = string
+  description = "A Public IP Address name which should be associated with the bastion host resource."
+}
+
+variable "public_ip_allocation_method" {
+  type        = string
+  description = "Public IP address allocation method - Static or Dynamic"
+  default     = "Static"
+}
+
+variable "public_ip_sku" {
+  type        = string
+  description = "The SKU which should be used. At this time the only supported value is Standard. Defaults to Standard."
+  default     = "Standard"
+}
+
+variable "public_ip_zones" {
+  type        = list(string)
+  description = "Specifies a list of public ip zones in which this bastion host should be located."
+  default     = ["1"]
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "The bastion host subnet name"
+  default     = "AzureBastionSubnet"
+}
+
 
