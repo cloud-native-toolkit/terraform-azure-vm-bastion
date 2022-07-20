c7fb7cde996f8bea03a0d3a325f95bf8d77b209e
hemang-ibm <103431190+hemang-ibm@users.noreply.github.com>
2022-07-20 12:15:54 +0530

    Update stage1-subnet.tf
    subnet name hardcoded for bastion host

--- test/stages/stage1-subnet.tf
+++ test/stages/stage1-subnet.tf
@@ -4,8 +4,9 @@ module "subnets" {
   resource_group_name = module.resource_group.name
   region              = var.region
   vpc_name = module.vnet.name
-  _count = 3
-  ipv4_cidr_blocks = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  subnet_name = "AzureBastionSubnet"
+  _count = 1
+  ipv4_cidr_blocks = ["10.1.1.0/24"]
   acl_rules = [{
     name = "ssh-inbound"
     action = "Allow"
