d4513dd1e2938f0363263f49c081e81ca2eb2739
hemang-ibm <103431190+hemang-ibm@users.noreply.github.com>
2022-07-20 11:47:55 +0530

    Update main.tf
    to have default values for ip configuration and public ip - changes done

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
+locals {
+  public_ip_name_prefix = "${var.bastion_host_name}-ip-config"
+  ip_config_name_prefix = "${var.bastion_host_name}-pip"
+}
 
 resource "azurerm_public_ip" "public_ip" {
-  name                = var.public_ip_name
+  name                = var.public_ip_name != "" ? var.public_ip_name : local.public_ip_name_prefix
   location            = var.region
   resource_group_name = var.resource_group_name
   allocation_method   = var.public_ip_allocation_method
@@ -14,7 +18,7 @@ resource "azurerm_bastion_host" "bastion_host" {
   resource_group_name = var.resource_group_name
 
   ip_configuration {
-    name = var.ip_configuration_name    
+    name                 = var.ip_configuration_name != "" ? var.ip_configuration_name : local.ip_config_name_prefix
     subnet_id            = var.subnet_id
     public_ip_address_id = azurerm_public_ip.public_ip.id
   }
