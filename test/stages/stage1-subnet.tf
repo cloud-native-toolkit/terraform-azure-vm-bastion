798889e6d60e2a987ec1a875d76d8ec66212657c
hemang-ibm <103431190+hemang-ibm@users.noreply.github.com>
2022-07-20 12:07:07 +0530

    Update stage1-subnet.tf
    subnet test case parameters updated as per new subnet changes

--- test/stages/stage1-subnet.tf
+++ test/stages/stage1-subnet.tf
@@ -3,28 +3,29 @@ module "subnets" {
 
   resource_group_name = module.resource_group.name
   region              = var.region
-  vpc_name            = module.vnet.name
-  subnet_name         = "AzureBastionSubnet"
-  _count              = 1
-  ipv4_cidr_blocks    = ["10.1.1.0/24"]
+  vpc_name = module.vnet.name
+  _count = 3
+  ipv4_cidr_blocks = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
   acl_rules = [{
-    name        = "ssh-inbound"
-    action      = "Allow"
-    direction   = "Inbound"
-    source      = "*"
+    name = "ssh-inbound"
+    action = "Allow"
+    direction = "Inbound"
+    source = "*"
     destination = "*"
     tcp = {
-      port_min        = 22
-      port_max        = 22
-      source_port_min = 22
-      source_port_max = 22
+      destination_port_range = "22"
+      source_port_range = "*"
     }
-
+  }, {
+    name = "vpn-inbound"
+    action = "Allow"
+    direction = "Inbound"
+    source = "*"
+    destination = "*"
+    udp = {
+      destination_port_range = "1194"
+      source_port_range = "*"
+    }
   }]
 }
 
-resource "null_resource" "print_enabled" {
-  provisioner "local-exec" {
-    command = "echo -n '${module.subnets.enabled}' > .enabled"
-  }
-}
